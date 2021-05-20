98320bce892637ef6926386e152c93eedd413a7c
arzmuda <63482173+arzmuda@users.noreply.github.com>
2021-05-20 12:17:08 +0200

    Update Prisoner.java

--- src/Prisoner.java
+++ src/Prisoner.java
@@ -1,3 +1,25 @@
-public class Prisoner {
+class Prisoner {
 
-}
+public:
+    string name;
+    string surname;
+
+    bool czyMoglbyEwentualnieBycPodejrzany() {}
+
+
+    string getName()  {
+        return name;
+    }
+
+    string setName(string name ) {
+        Prisoner::name = name;
+    }
+
+    string getSurname() {
+        return surname;
+    }
+
+    string setSurname(string Surname) {
+        Prisoner::surname = surname;
+    }
+};
