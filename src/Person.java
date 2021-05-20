82cd2f158f1003516a9822f09710d5968c13cf5f
Kacper Rzymkiewicz <kacper9800@gmail.com>
2021-05-20 12:07:53 +0200

    Person

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,36 @@
 public class Person {
 
+  private int age;
+  private String firstName;
+  private String middleName;
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+
+  public String display() {
+    return "Wiek:" + age + ", Imie: " + firstName + ", Drugie imie: " + middleName;
+
+  }
+
 }
